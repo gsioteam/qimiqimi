55bceee4454749d1ae88854bc7898c7de32726bb
gsio <gsioteam@protonmail.com>
2021-11-01 14:46:15 +0700

    Commit v2

--- index.js
+++ index.js
@@ -1,115 +1,40 @@
-
-const {Collection} = require('./collection');
-
-class HomeCollection extends Collection {
-
-    reload(_, cb) {
-        let pageUrl = new PageURL(this.url);
-        this.fetch(this.url).then((doc)=>{
-            let titles = doc.querySelectorAll('.latest-tab-nav > ul > li');
-            let cols = doc.querySelectorAll('.latest-tab-box .latest-item');
-            let len = titles.length;
-
-            let items = [];
-            for (let i = 0; i < len; ++i) {
-                let telem = titles[i];
-                let item = glib.DataItem.new();
-                item.type = glib.DataItem.Type.Header;
-                item.title = telem.text;
-                items.push(item);
-
-                let celem = cols[i];
-                let list = celem.querySelectorAll('.img-list > li');
-                for (let node of list) {
-                    let link = node.querySelector('a.play-img');
-                    let img = link.querySelector('img');
-                    let item = glib.DataItem.new();
-                    item.title = link.attr('title');
-                    item.link = pageUrl.href(link.attr('href'));
-                    item.picture = img.attr('src');
-                    item.subtitle = node.querySelector('.time').text;
-                    items.push(item);
+class IndexController extends Controller {
+    load() {
+        this.data = {
+            tabs: [
+                {
+                    "title": "首页",
+                    "id": "home",
+                    "url": "http://www.qimiqimi.co/"
+                },
+                {
+                    "title": "新番连载",
+                    "id": "xinfan",
+                    "url": "http://www.qimiqimi.co/type/xinfan/page/{0}.html"
+                },
+                {
+                    "title": "完结日漫",
+                    "id": "riman",
+                    "url": "http://www.qimiqimi.co/type/riman/page/{0}.html"
+                },
+                {
+                    "title": "BD番组",
+                    "id": "wuxiu",
+                    "url": "http://www.qimiqimi.co/type/wuxiu/page/{0}.html"
+                },
+                {
+                    "title": "热门国漫",
+                    "id": "guoman",
+                    "url": "http://www.qimiqimi.co/type/guoman/page/{0}.html"
+                },
+                {
+                    "title": "剧场&OVA",
+                    "id": "jcdm",
+                    "url": "http://www.qimiqimi.co/type/jcdm/page/{0}.html"
                 }
-            }
-            this.setData(items);
-            cb.apply(null);
-        }).catch((err)=>{
-            if (err instanceof Error) {
-                console.log("Err " + err.message + " stack " + err.stack);
-                err = glib.Error.new(305, err.message);
-            }
-            cb.apply(err);
-        });
-        return true;
+            ]
+        };
     }
 }
 
-class CategoryCollection extends Collection {
-
-    constructor(data) {
-        super(data);
-        this.page = 0;
-    }
-
-    async fetch(url) {
-        let pageUrl = new PageURL(url);
-
-        let doc = await super.fetch(url);
-        let elems = doc.querySelectorAll('.img-list > li > a');
-
-        let results = [];
-
-        for (let i = 0, t = elems.length; i < t; ++i) {
-            let elem = elems[i];
-
-            let item = glib.DataItem.new();
-
-            item.title = elem.attr('title');
-            item.link = pageUrl.href(elem.attr('href'));
-            let img = elem.querySelector('img');
-            item.picture = pageUrl.href(img.attr('src'));
-            item.subtitle = elem.querySelector('p').text.trim();
-            results.push(item);
-        }
-        return results;
-    }
-
-    makeURL(page) {
-        return this.url.replace('{0}', page + 1);
-    }
-
-    reload(_, cb) {
-        let page = 0;
-        this.fetch(this.makeURL(page)).then((results)=>{
-            this.page = page;
-            this.setData(results);
-            cb.apply(null);
-        }).catch(function(err) {
-            if (err instanceof Error) 
-                err = glib.Error.new(305, err.message);
-            cb.apply(err);
-        });
-        return true;
-    }
-
-    loadMore(cb) {
-        let page = this.page + 1;
-        this.fetch(this.makeURL(page)).then((results)=>{
-            this.page = page;
-            this.appendData(results);
-            cb.apply(null);
-        }).catch(function(err) {
-            if (err instanceof Error) 
-                err = glib.Error.new(305, err.message);
-            cb.apply(err);
-        });
-        return true;
-    }
-}
-
-module.exports = function(info) {
-    let data = info.toObject();
-    if (data.id === 'home') 
-        return HomeCollection.new(data);
-    else return CategoryCollection.new(data);
-};
+module.exports = IndexController;
--- search.js
+++ search.js
@@ -1,68 +1,168 @@
 
-const {Collection} = require('./collection');
+const baseURL = 'http://www.qimiqimi.co/index.php/vod/search/wd/{0}/page/{1}.html';
 
-class SearchCollection extends Collection {
-    
-    constructor(data) {
-        super(data);
-        this.page = 0;
+class SearchController extends Controller {
+
+    load() {
+        let str = localStorage['hints'];
+        let hints = [];
+        if (str) {
+            let json = JSON.parse(str);
+            if (json.push) {
+                hints = json;
+            }
+        }
+        this.data = {
+            list: [],
+            focus: false,
+            hints: hints,
+            text: '',
+            loading: false,
+        };
+        this.hasMore = true;
     }
 
-    async fetch(url) {
-        let pageUrl = new PageURL(url);
-        let doc = await super.fetch(url);
+    makeURL(word, page) {
+        return baseURL.replace('{0}', encodeURIComponent(word)).replace('{1}', page + 1);
+    }
+
+    onSearchClicked() {
+        this.findElement('input').submit();
+    } 
+
+    onTextChange(text) {
+        console.log("onTextChange " + text);
+        this.data.text = text;
+    }
+
+    async onTextSubmit(text) {
+        console.log("onTextSubmit " + text);
+        let hints = this.data.hints;
+        if (text.length > 0) {
+            if (hints.indexOf(text) < 0) {
+                this.setState(()=>{
+                    hints.unshift(text);
+                    while (hints.length > 30) {
+                        hints.pop();
+                    }
+    
+                    localStorage['hints'] = JSON.stringify(hints);
+                });
+            }
+            
+            this.setState(()=>{
+                this.data.loading = true;
+            });
+            try {
+                let list = await this.request(this.makeURL(text, 0));
+                this.key = text;
+                this.page = 0;
+                this.hasMore = true;
+                this.setState(()=>{
+                    this.data.list = list;
+                    this.data.loading = false;
+                });
+            } catch(e) {
+                showToast(`${e}\n${e.stack}`);
+                this.setState(()=>{
+                    this.data.loading = false;
+                });
+            }
+        }
+    }
+
+    onTextFocus() {
+        this.setState(()=>{
+            this.data.focus = true;
+        });
+    }
+
+    onTextBlur() {
+        this.setState(()=>{
+            this.data.focus = false;
+        });
+    }
+
+    onPressed(index) {
+        var data = this.data.list[index];
+        console.log(`test ${data.link}`);
+        openVideo(data.link, data);
+    }
+
+    onHintPressed(index) {
+        let hint = this.data.hints[index];
+        if (hint) {
+            this.setState(()=>{
+                this.data.text = hint;
+                this.findElement('input').blur();
+                this.onTextSubmit(hint);
+            });
+        }
+    }
+
+    async onRefresh() {
+        let text = this.key;
+        if (!text) return;
+        try {
+            let list = await this.request(this.makeURL(text, 0));
+            this.page = 0;
+            this.hasMore = true;
+            this.setState(()=>{
+                this.data.list = list;
+                this.data.loading = false;
+            });
+        } catch(e) {
+            showToast(`${e}\n${e.stack}`);
+            this.setState(()=>{
+                this.data.loading = false;
+            });
+        }
+    }
+
+    async onLoadMore() {
+        if (!this.hasMore) return;
+        let page = this.page + 1;
+        try {
+            let list = await this.request(this.makeURL(text, page));
+            if (list.length == 0) {
+                this.hasMore = false;
+            }
+            this.page = page;
+            this.setState(()=>{
+                for (let item in list) {
+                    this.data.list.push(item);
+                }
+                this.data.loading = false;
+            });
+        } catch(e) {
+            showToast(`${e}\n${e.stack}`);
+            this.setState(()=>{
+                this.data.loading = false;
+            });
+        }
+    }
+
+    async request(url) {
+        let res = await fetch(url);
+        let html = await res.text();
+        let doc = HTMLParser.parse(html);
         let nodes = doc.querySelectorAll('.show-list > li');
 
         let results = [];
         for (let node of nodes) {
-            let item = glib.DataItem.new();
             let elem = node.querySelector('.play-img');
             let link = elem.querySelector('img');
 
-            item.link = pageUrl.href(elem.querySelector('a').attr('href'));
-            item.title = link.attr('alt');
-            item.picture = link.attr('src');
-            item.subtitle = node.querySelector('.play-txt .fn-right').text.trim();
+            let item = {
+                link: new URL(elem.getAttribute('href'), url).toString(),
+                title: link.getAttribute('alt'),
+                picture: link.getAttribute('src'),
+                subtitle: node.querySelector('.play-txt .fn-right').text.trim(),
+            };
             results.push(item);
         }
         return results;
     }
-
-    makeURL(page) {
-        return this.url.replace('{0}', glib.Encoder.urlEncode(this.key)).replace('{1}', page + 1);
-    }
-
-    reload(data, cb) {
-        this.key = data.get("key") || this.key;
-        let page = data.get("page") || 0;
-        if (!this.key) return false;
-        this.fetch(this.makeURL(page)).then((results)=>{
-            this.page = page;
-            this.setData(results);
-            cb.apply(null);
-        }).catch(function(err) {
-            if (err instanceof Error) 
-                err = glib.Error.new(305, err.message);
-            cb.apply(err);
-        });
-        return true;
-    } 
-
-    loadMore(cb) {
-        let page = this.page + 1;
-        this.fetch(this.makeURL(page)).then((results)=>{
-            this.page = page;
-            this.appendData(results);
-            cb.apply(null);
-        }).catch(function(err) {
-            if (err instanceof Error) 
-                err = glib.Error.new(305, err.message);
-            cb.apply(err);
-        });
-        return true;
-    }
 }
 
-module.exports = function(data) {
-    return SearchCollection.new(data ? data.toObject() : {});
-};
+module.exports = SearchController;
